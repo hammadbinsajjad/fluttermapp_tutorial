c8f0a8ae197d1750144282e70fb90cbb8660864e
Hammad Bin Sajjad <hammadbinsajjad@outlook.com>
2022-11-12 16:15:07 +0500

    Learn Flutter Page complete

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -8,6 +8,9 @@ class LearnFlutterPage extends StatefulWidget {
 }
 
 class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  bool isSwitch = false;
+  bool? isCheckBox = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,64 +23,96 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           },
           icon: const Icon(Icons.arrow_back),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              debugPrint("Actions");
+            },
+            icon: const Icon(Icons.info_outline),
+          )
+        ],
       ),
-      body: Column(children: [
-        Image.asset("images/einstein.jpg"),
-        const SizedBox(height: 10),
-        const Divider(
-          color: Colors.black,
-        ),
-        Container(
-          margin: const EdgeInsets.all(10.0),
-          padding: const EdgeInsets.all(10.0),
-          width: double.infinity,
-          color: Colors.blueGrey,
-          child: const Center(
-            child: Text(
-              "This is a text widget",
-              style: TextStyle(color: Colors.white),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Image.asset("images/einstein.jpg"),
+          const SizedBox(height: 10),
+          const Divider(
+            color: Colors.black,
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
+            width: double.infinity,
+            color: Colors.blueGrey,
+            child: const Center(
+              child: Text(
+                "This is a text widget",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
           ),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            debugPrint("Elevated Button Clicked of Learn Flutter Page");
-          },
-          child: const Text("Elevated Button"),
-        ),
-        OutlinedButton(
-          onPressed: () {
-            debugPrint("Elevated Button Clicked of Learn Flutter Page");
-          },
-          child: const Text("Elevated Button"),
-        ),
-        TextButton(
-          onPressed: () {
-            debugPrint("Elevated Button Clicked of Learn Flutter Page");
-          },
-          child: const Text("Elevated Button"),
-        ),
-        GestureDetector(
-          behavior: HitTestBehavior.opaque,
-          onTap: () {
-            debugPrint("This is the row");
-          },
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              Icon(
-                Icons.local_fire_department,
-                color: Colors.blue,
-              ),
-              Text("Row Widget"),
-              Icon(
-                Icons.local_pharmacy,
-                color: Colors.blue,
-              ),
-            ],
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                // primary: isSwitch ? Colors.blue : Colors.green), Deprecated
+                // Use the line below for colour switching
+                backgroundColor: isSwitch ? Colors.blue : Colors.green),
+            onPressed: () {
+              debugPrint("Elevated Button Clicked of Learn Flutter Page");
+            },
+            child: const Text("Elevated Button"),
           ),
-        ),
-      ]),
+          OutlinedButton(
+            onPressed: () {
+              debugPrint("Elevated Button Clicked of Learn Flutter Page");
+            },
+            child: const Text("Elevated Button"),
+          ),
+          TextButton(
+            onPressed: () {
+              debugPrint("Elevated Button Clicked of Learn Flutter Page");
+            },
+            child: const Text("Elevated Button"),
+          ),
+          GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () {
+              debugPrint("This is the row");
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: const [
+                Icon(
+                  Icons.local_fire_department,
+                  color: Colors.blue,
+                ),
+                Text("Row Widget"),
+                Icon(
+                  Icons.local_pharmacy,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+          ),
+          Switch(
+            value: isSwitch,
+            onChanged: (bool newBool) {
+              setState(() {
+                isSwitch = newBool;
+              });
+            },
+          ),
+          Checkbox(
+              value: isCheckBox,
+              onChanged: (bool? newBool) {
+                setState(() {
+                  isCheckBox = newBool;
+                });
+              }),
+          Image.network(
+            "https://images.pexels.com/photos/1289845/pexels-photo-1289845.jpeg?auto=compress&cs=tinysrgb&w=720&h=300&dpr=1",
+          ),
+        ]),
+      ),
     );
   }
 }
