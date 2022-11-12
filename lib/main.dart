a88b97dd0bf1aeec5ac91a1cc3edfba313f40e9b
Hammad Bin Sajjad <hammadbinsajjad@outlook.com>
2022-11-12 12:14:32 +0500

    Added pages and home button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttermapp_tutorial/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,6 +34,7 @@ class _RootPageState extends State<RootPage> {
       appBar: AppBar(
         title: const Text("Flutter App"),
       ),
+      body: const HomePage(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           debugPrint("Floating Action Button Clicked");
