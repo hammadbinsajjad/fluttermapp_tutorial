78b7f261a66d914e2730f787578cb676a8168f8b
Hammad Bin Sajjad <hammadbinsajjad@outlook.com>
2022-11-12 13:24:55 +0500

    Implemented To and back from page on button click

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -10,6 +10,17 @@ class LearnFlutterPage extends StatefulWidget {
 class _LearnFlutterPageState extends State<LearnFlutterPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Learn Flutter"),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(Icons.arrow_back),
+        ),
+      ),
+    );
   }
 }
